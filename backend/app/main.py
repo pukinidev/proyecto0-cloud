b00a22c43ede857983e0ebcc1ab979d20ddca295
pukinidev <v.caro@uniandes.edu.co>
2025-01-23 11:14:36 -0500

    new files!

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from app.db.base import Base
 from app.db.config import engine
-from app.routes import user_route, task_route, category_route
+from app.routes import user_route, task_route, category_route, auth_route
 
 app = FastAPI(
     title="Tasks API",
@@ -21,6 +21,7 @@ Base.metadata.create_all(bind=engine)
 app.include_router(user_route.router, prefix="/user")
 app.include_router(task_route.router, prefix="/task")
 app.include_router(category_route.router, prefix="/category")
+app.include_router(auth_route.router, prefix="/auth")
 
 @app.get("/")
 def read_root():
