3317ad1de835d811e6476ace8eddd9b328e43637
pukinidev <v.caro@uniandes.edu.co>
2025-01-22 21:27:33 -0500

    docker files 🐬

--- backend/app/db/config.py
+++ backend/app/db/config.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./db.db"
+DATABASE_URL_SQLITE = "sqlite:///./db.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = "postgresql://postgres:postgres@database:5432/postgres"
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
